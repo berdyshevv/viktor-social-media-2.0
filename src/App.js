cb6c9b84d625d3609702dc04d17994e206bdab7f
berdyshevv <viktorberdysh@gmail.com>
2022-09-18 21:11:26 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
 
 function App() {
   const [index, setIndex] = useState(0);
@@ -10,10 +11,23 @@ function App() {
 
   return (
     <div className="App">
-      {index}
-      <div>
-        <button onClick={onButtonClickNext}>next</button>
-      </div>
+      <Card style={{ width: "51rem" }}>
+        <Card.Img
+          variant="top"
+          src="holder.js/100px180"
+          style={{ height: "25rem" }}
+        />
+        <Card.Body>
+          <Card.Title>{index}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="outline-secondary" onClick={onButtonClickNext}>
+            Go somewhere
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }

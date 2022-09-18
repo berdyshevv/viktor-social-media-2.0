5103f6b4f71c17be20211129960348e9aa5adbf9
berdyshevv <viktorberdysh@gmail.com>
2022-09-18 16:25:35 +0300

    simple counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const onButtonClickNext = () => {
+      setIndex(index => index + 1)
+
+  }
+
   return (
     <div className="App">
-      hello
+      { index }
+      <div>
+        <button onClick={onButtonClickNext}>next</button>
+      </div>
     </div>
   );
 }
